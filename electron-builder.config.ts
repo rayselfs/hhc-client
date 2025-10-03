70e0cebdeb702095a438ee7f5215cede35b7a18e
Ray.CJ Chang <ray.cj_chang@htc.com>
2025-10-03 16:33:05 +0800

    feat: add ignore apple sign

--- electron-builder.config.ts
+++ electron-builder.config.ts
@@ -17,7 +17,26 @@ const config: Configuration = {
     repo: 'hhc-client',
   },
 
-  mac: { target: 'dmg' },
+  mac: {
+    target: [
+      {
+        target: 'dmg',
+        arch: ['x64', 'arm64'],
+      },
+      {
+        target: 'zip',
+        arch: ['x64', 'arm64'],
+      },
+    ],
+    category: 'public.app-category.utilities',
+    hardenedRuntime: true,
+    gatekeeperAssess: false,
+    entitlements: 'build/entitlements.mac.plist',
+    entitlementsInherit: 'build/entitlements.mac.plist',
+    // 如果沒有 Apple Developer ID，則跳過簽名
+    identity: process.env.CSC_NAME || null,
+    notarize: !!process.env.APPLE_ID,
+  },
   win: { target: 'nsis' },
   linux: { target: 'AppImage' },
 }
